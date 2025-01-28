4281b9578493f59245bc9e6b4b63de953e03fe34
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-27 23:33:19 -0300

    feat: add slash scroll effect

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,9 +1,39 @@
-"use client"
+import { useState, useEffect } from "react"
 
 export default function HeroSection() {
+  const [scrollY, setScrollY] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    }
+
+    let ticking = false
+    const throttledHandleScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          handleScroll()
+          ticking = false
+        })
+        ticking = true
+      }
+    }
+
+    window.addEventListener("scroll", throttledHandleScroll)
+    return () => window.removeEventListener("scroll", throttledHandleScroll)
+  }, [])
+
+ 
+  const clipPathPercentage = Math.min(80 + scrollY * 0.05, 100) 
+  const clipPath = `polygon(0 0, 100% 0, 100% ${clipPathPercentage}%, 0 100%)`
+
   return (
-    <section className="w-full h-screen bg-blue-700 text-white overflow-hidden  px-5 lg:px-10 flex flex-col justify-center" style={{ clipPath: "polygon(0 0, 100% 0, 100% 90%, 0 100%)" }}>
-      <div className="container mx-auto px-4 py-8 md:py-32 ">
+    <section
+      id="/"
+      className="w-full h-screen bg-blue-700 text-white overflow-hidden px-5 lg:px-10 flex flex-col justify-center"
+      style={{ clipPath }}
+    >
+      <div className="container mx-auto px-4 py-8 md:py-32">
         <h1 className="text-3xl md:text-6xl md:leading-tight font-bold mb-6">
           Descubra por que o Pesquisa Ativa está revolucionando o mundo dos negócios!
         </h1>
