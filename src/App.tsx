dda7d379c466fd147f5f4cf5bdc34114abce830a
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-27 23:53:38 -0300

    feat: add header, hero-section, info-section, solutions-section, data-security-section, contact-section and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import ContactSection from "./components/ContactSection"
+import DataSecuritySection from "./components/DataSecuritySection"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
+import InfoSection from "./components/InfoSection"
+import SolutionsSection from "./components/SolutionSection"
+
 function App() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline"></h1>
-    </div>
+    <main className="w-full flex min-h-screen flex-col items-center justify-between">
+      <Header />
+      <HeroSection />
+      <InfoSection />
+      <SolutionsSection />
+      <DataSecuritySection />
+      <ContactSection />
+      <Footer />
+    </main>
   )
 }
 export default App
